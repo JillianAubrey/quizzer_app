b73023efb99f95166996ea166481af97f4a1e16e
Jeremy Buist <79812985+jbuistjbuist@users.noreply.github.com>
2022-10-21 19:54:12 -0400

    changing routing and DB files to suit our project

---
+++ db/queries/api.js
@@ -0,0 +1,5 @@
+//file for API queries (may share some functionality with db/users.js but trying to modularize).
+
+const db = require('../connection'); //connect to DB
+
+module.exports = {}; //nothing to export..yet
