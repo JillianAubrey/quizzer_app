6930a84d6d07ee4d1b3c75380d1551eac996c3c7
JillianAubrey <jillian.nk.aubrey@gmail.com>
2022-10-21 23:27:11 +0000

    Modify users schema/seeds to reflect project needs. Remove widgets schema/seeds

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,5 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (name, email, password) VALUES ('Jillian', 'jillian@quizzes.com', 'password');
+INSERT INTO users (name, email, password) VALUES ('Jeremy', 'jeremy@quizzes.com', 'password');
+INSERT INTO users (name, email, password) VALUES ('Alice', 'alice@quizzes.com', 'password');
+INSERT INTO users (name, email, password) VALUES ('Kira', 'kira@quizzes.com', 'password');
