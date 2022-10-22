00fca43ba0d2978cb5a77ee563b620a083439800
JillianAubrey <jillian.nk.aubrey@gmail.com>
2022-10-22 01:33:58 +0000

    Create seed for attempts table

---
+++ db/seeds/05_attempts.sql
@@ -0,0 +1,12 @@
+INSERT INTO attempts (quiz_id, user_id, attempted_at, url)
+VALUES (1, 2, '2022-10-17', 'pd1SXflkqm');
+INSERT INTO attempts (quiz_id, user_id, attempted_at, url)
+VALUES (1, 3, '2022-10-18', 'EZdbomSxaM');
+INSERT INTO attempts (quiz_id, user_id, attempted_at, url)
+VALUES (1, 4, '2022-10-19', 'yxNX4Xff0Q');
+INSERT INTO attempts (quiz_id, user_id, attempted_at, url)
+VALUES (2, 1, '2022-10-20', '8Esbdnmlan');
+INSERT INTO attempts (quiz_id, user_id, attempted_at, url)
+VALUES (2, 3, '2022-10-21', 'OYe70s3LUb');
+INSERT INTO attempts (quiz_id, user_id, attempted_at, url)
+VALUES (3, 2, '2022-10-22', 'QWWmLQRon7');
