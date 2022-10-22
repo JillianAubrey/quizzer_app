fb99e4f81163f3cd9a41c6d09bd3e8ba980a1e45
JillianAubrey <jillian.nk.aubrey@gmail.com>
2022-10-22 01:35:38 +0000

    Create seed for attempt_answers table

---
+++ db/seeds/06_attempt_answers.sql
@@ -0,0 +1,40 @@
+INSERT INTO attempt_answers (attempt_id, answer_id)
+VALUES (1, 2);
+INSERT INTO attempt_answers (attempt_id, answer_id)
+VALUES (1, 4);
+INSERT INTO attempt_answers (attempt_id, answer_id)
+VALUES (1, 9);
+INSERT INTO attempt_answers (attempt_id, answer_id)
+VALUES (1, 10);
+INSERT INTO attempt_answers (attempt_id, answer_id)
+VALUES (2, 1);
+INSERT INTO attempt_answers (attempt_id, answer_id)
+VALUES (2, 4);
+INSERT INTO attempt_answers (attempt_id, answer_id)
+VALUES (2, 7);
+INSERT INTO attempt_answers (attempt_id, answer_id)
+VALUES (2, 11);
+INSERT INTO attempt_answers (attempt_id, answer_id)
+VALUES (3, 2);
+INSERT INTO attempt_answers (attempt_id, answer_id)
+VALUES (3, 4);
+INSERT INTO attempt_answers (attempt_id, answer_id)
+VALUES (3, 7);
+INSERT INTO attempt_answers (attempt_id, answer_id)
+VALUES (3, 12);
+INSERT INTO attempt_answers (attempt_id, answer_id)
+VALUES (4, 14);
+INSERT INTO attempt_answers (attempt_id, answer_id)
+VALUES (4, 17);
+INSERT INTO attempt_answers (attempt_id, answer_id)
+VALUES (4, 18);
+INSERT INTO attempt_answers (attempt_id, answer_id)
+VALUES (5, 14);
+INSERT INTO attempt_answers (attempt_id, answer_id)
+VALUES (5, 17);
+INSERT INTO attempt_answers (attempt_id, answer_id)
+VALUES (5, 19);
+INSERT INTO attempt_answers (attempt_id, answer_id)
+VALUES (6, 22);
+INSERT INTO attempt_answers (attempt_id, answer_id)
+VALUES (6, 23);
