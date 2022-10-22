6c858d16d4b582c6cd86de1e5ae28eb4fea06f6b
JillianAubrey <jillian.nk.aubrey@gmail.com>
2022-10-22 01:18:30 +0000

    Create seed data for quizzes table

--- db/seeds/02_quizzes.sql
+++ db/seeds/02_quizzes.sql
@@ -1,1 +1,6 @@
-
+INSERT INTO quizzes (user_id,	title,	description,	url,	results_url,	created_at,	is_private)
+VALUES (1,	'Provincial Capitals',	'How well do you know the capitals of Canada''s provinces?',	'YKZRJPgoBo',	'2wW9XCqK01',	'2022-10-22', FALSE);
+INSERT INTO quizzes (user_id,	title,	description,	url,	results_url,	created_at,	is_private)
+VALUES (2,	'SQL Commands',	'A quiz on some of the basic commands in SQL',	'QPgYVRfbBx',	'F2PRKteq6k',	'2022-10-20',	FALSE);
+INSERT INTO quizzes (user_id,	title,	description,	url,	results_url,	created_at,	is_private)
+VALUES (1,	'My cats',	'All about my cats: Dawson and Koshu 🐱🐱',	'uSqMnu3N1r',	's9Bew9sJHA',	'2022-10-19',	TRUE);
