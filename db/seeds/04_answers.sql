37de7eef4268568a6156589276dc41dd23b24cb4
JillianAubrey <jillian.nk.aubrey@gmail.com>
2022-10-22 01:30:28 +0000

    Create seed for answers table

---
+++ db/seeds/04_answers.sql
@@ -0,0 +1,48 @@
+INSERT INTO answers (question_id, text, is_correct)
+VALUES (1, 'Ottawa', FALSE);
+INSERT INTO answers (question_id, text, is_correct)
+VALUES (1, 'Toronto', TRUE);
+INSERT INTO answers (question_id, text, is_correct)
+VALUES (1, 'Sudbury', FALSE);
+INSERT INTO answers (question_id, text, is_correct)
+VALUES (2, 'Quebec City', TRUE);
+INSERT INTO answers (question_id, text, is_correct)
+VALUES (2, 'Montreal', FALSE);
+INSERT INTO answers (question_id, text, is_correct)
+VALUES (2, 'Trois Riveres', FALSE);
+INSERT INTO answers (question_id, text, is_correct)
+VALUES (3, 'Calgary', FALSE);
+INSERT INTO answers (question_id, text, is_correct)
+VALUES (3, 'Red Deer', FALSE);
+INSERT INTO answers (question_id, text, is_correct)
+VALUES (3, 'Edmonton', TRUE);
+INSERT INTO answers (question_id, text, is_correct)
+VALUES (4, 'Fredericton', TRUE);
+INSERT INTO answers (question_id, text, is_correct)
+VALUES (4, 'Moncton', FALSE);
+INSERT INTO answers (question_id, text, is_correct)
+VALUES (4, 'Saint John', FALSE);
+INSERT INTO answers (question_id, text, is_correct)
+VALUES (5, 'ADD TO table (column_names) VALUES (values)', FALSE);
+INSERT INTO answers (question_id, text, is_correct)
+VALUES (5, 'INSERT INTO table (column_names) VALUES (values)', TRUE);
+INSERT INTO answers (question_id, text, is_correct)
+VALUES (6, 'DELETE TABLE table', FALSE);
+INSERT INTO answers (question_id, text, is_correct)
+VALUES (6, 'rm -r table', FALSE);
+INSERT INTO answers (question_id, text, is_correct)
+VALUES (6, 'DROP TABLE table', TRUE);
+INSERT INTO answers (question_id, text, is_correct)
+VALUES (7, 'HAVING condition', TRUE);
+INSERT INTO answers (question_id, text, is_correct)
+VALUES (7, 'WHERE condition', FALSE);
+INSERT INTO answers (question_id, text, is_correct)
+VALUES (7, 'WITH condition', FALSE);
+INSERT INTO answers (question_id, text, is_correct)
+VALUES (3, 'Dawson', FALSE);
+INSERT INTO answers (question_id, text, is_correct)
+VALUES (3, 'Koshu', TRUE);
+INSERT INTO answers (question_id, text, is_correct)
+VALUES (4, 'Dawson', TRUE);
+INSERT INTO answers (question_id, text, is_correct)
+VALUES (4, 'Koshu', FALSE);
