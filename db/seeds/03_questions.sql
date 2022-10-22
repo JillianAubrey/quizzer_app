8e9b42403b13f89c0b3b6691f6536ea956fc505d
JillianAubrey <jillian.nk.aubrey@gmail.com>
2022-10-22 01:24:45 +0000

    Create seed for questions table

---
+++ db/seeds/03_questions.sql
@@ -0,0 +1,18 @@
+INSERT INTO questions (quiz_id, text, sequence)
+VALUES (1, 'What''s the capital of Ontario?', 1);
+INSERT INTO questions (quiz_id, text, sequence)
+VALUES (1, 'What''s the capital of Quebec?', 2);
+INSERT INTO questions (quiz_id, text, sequence)
+VALUES (1, 'What''s the capital of Alberta?', 3);
+INSERT INTO questions (quiz_id, text, sequence)
+VALUES (1, 'What''s the capital of New Brunswick?', 5);
+INSERT INTO questions (quiz_id, text, sequence)
+VALUES (2, 'What''s the command for adding a row to a table?', 2);
+INSERT INTO questions (quiz_id, text, sequence)
+VALUES (2, 'What''s the command for deleting a table?', 3);
+INSERT INTO questions (quiz_id, text, sequence)
+VALUES (2, 'What command do I use to filter by an aggregate column?', 1);
+INSERT INTO questions (quiz_id, text, sequence)
+VALUES (3, 'Which cat is younger?', 1);
+INSERT INTO questions (quiz_id, text, sequence)
+VALUES (3, 'Which cat has long hair?', 2);
