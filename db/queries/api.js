bf2663558798c0302eaab5a3dbaeaa0dfcd1c618
Jeremy Buist <79812985+jbuistjbuist@users.noreply.github.com>
2022-10-24 16:47:33 -0400

    adding functions to insert all the answers into the database

--- db/queries/api.js
+++ db/queries/api.js
@@ -105,8 +105,9 @@ const addQuiz = function(userId, content) {
 
   return db.query(quizzesQuery, [userId, quizTitle, quizDescription, url, resultsUrl, quizPrivate])
     .then((quizData) => addQuestions(quizData.rows[0].id, content))
-    .then((ansData) => addAnswers(ansData))
-   // .then((answerData))
+    .then((questionData) => addAnswers(questionData, content))
+    .then((answerData) => console.log(answerData.rows))
+    .catch(error => console.log(error));
 }
 
 const addQuestions = function(quizId, content) {
@@ -119,7 +120,6 @@ const addQuestions = function(quizId, content) {
   for (let key in content) {
     if (key.length === 1) {
       queryParams.push(`${quizId}`, content[key], key);
-      console.log('query params: ', queryParams)
       questionQuery += `($${queryCount}, $${queryCount + 1}, $${queryCount + 2}),`;
       queryCount += 3;
     }
@@ -128,35 +128,56 @@ const addQuestions = function(quizId, content) {
   questionQuery = questionQuery.slice(0, -1);
   questionQuery += ' RETURNING *;';
 
-  console.log(questionQuery);
-
   return db.query(questionQuery, queryParams)
     .then(data => data.rows);
 }
 
 const addAnswers = function(questions, content) {
-  
+
+  let ansQuery = `INSERT INTO answers(question_id, text, is_correct) VALUES `
+  let queryCount = 1;
+  let queryParams = [];
+  let answers = [];
+
+  questions.forEach((question) => {
+
+
+    for (let key in content) {
+      if (key[key.length - 1] === 'a') {
+        if (!answers.includes(content[key])) {
+          answers.push(content[key]);
+        }
+      }
+    }
+
+    for (let key in content) {
+      if (key.length >= 3 && key !== 'quiz_title' && key !== 'quiz_description' && key !== 'quiz_private') {
+
+        let quesAndAns = key.split('-');
+
+        if (quesAndAns[1] !== 'a' && Number(quesAndAns[0]) === question.sequence) {
+
+          ansQuery += `($${queryCount}, $${queryCount + 1}, $${queryCount + 2}),`
+          queryParams.push(question.id, content[key]);
+
+          if (answers.includes(key)) {
+            queryParams.push('TRUE')
+          } else {
+            queryParams.push('FALSE')
+          }
+
+          queryCount += 3;
+
+        }
+      }
+    }
+  })
+
+  ansQuery = ansQuery.slice(0, -1);
+  ansQuery += 'RETURNING *;';
+
+  return db.query(ansQuery, queryParams);
+
 }
-
-//information needed:
-//quizzes
-// user_id
-// title
-// description
-//url
-//results_url
-//created_at (default now())
-//is_private (default true)
-
-//questions
-//quiz_id
-//text
-//sequence
-
-//answers
-//question_id
-//text
-//is_correct
-
 
 module.exports = { getQuizzes, getQuizByUrl, addQuiz };
