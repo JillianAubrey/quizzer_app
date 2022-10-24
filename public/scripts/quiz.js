7589c8ff35e1e4620ee7854cbabd0a19cfb10d23
JillianAubrey <jillian.nk.aubrey@gmail.com>
2022-10-24 01:50:09 +0000

    Add code to submit button to compile selected answers. POSTing to be added later

--- public/scripts/quiz.js
+++ public/scripts/quiz.js
@@ -1,9 +1,23 @@
 $(() => {
   $('.question_card li').on('click', function() {
-    selectChildRadioButton($(this))});
+    selectChildRadioButton($(this))
+  });
+
+  $('#quiz_submit').on('click', () => {
+    submitQuiz($('.quiz_container'));
+  })
 });
 
 const selectChildRadioButton = ($element) => {
   $element.parent().find('input:radio').attr('checked', false)
   $element.find('input:radio').attr('checked', true);
+};
+
+const submitQuiz = ($quizContainer) => {
+  const answerIds = [];
+  $.each($quizContainer.find('input:checked'), (index, value) => {
+    const id = Number($(value).attr('data-id'));
+    answerIds.push(id);
+  })
+  // Once route exists, add code here to post the answerIds
 };
